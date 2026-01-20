15be96809366ce8fa6ee99b2579fd01ae4172475
arshzeelani <arshzeelani@gamil.com>
2026-01-20 12:08:57 +0530

    create a registrations page

---
+++ flutter_app_2/lib/info.dart
@@ -0,0 +1,38 @@
+// import 'package:flutter/material.dart';
+
+// class Login extends StatelessWidget {
+//   final String name;
+//   final String email;
+//   final String password;
+
+//   const Login({
+//     super.key,
+//     required this.name,
+//     required this.email,
+//     required this.password,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "login_new_page",
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(title: Text("login")),
+//         body: Column(
+//           children: [
+//             TextFormField(
+//               decoration: InputDecoration(labelText: "Username: $name"),
+//             ),
+//             TextFormField(
+//               decoration: InputDecoration(labelText: "Email_id: $email"),
+//             ),
+//             TextFormField(
+//               decoration: InputDecoration(labelText: "Password $password"),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
